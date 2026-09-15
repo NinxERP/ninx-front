7b7b996df284a376a0a25446b884fa35d53c19cc
Matheus Augusto Teixeira Silva <maataug.pessoal@gmail.com>
2026-09-14 23:48:31 -0300

    Dia de vencimento do fiado e versao definida pela tag
    - Campo "Dia de Vencimento do Fiado" na gestao do comercio
    - Versao do app lida do package.json; release grava a versao a partir da tag
    - Workflow de CI com checagem de tipos e testes

--- src/types/comercio.ts
+++ src/types/comercio.ts
@@ -10,6 +10,7 @@ export interface ComercioRequest {
   cnpj?: string;
   assinaturaResponsavelBase64?: string;
   limiteCreditoPadrao?: number;
+  diaVencimentoFiado?: number;
 }
 
 export interface ComercioResponse {
@@ -25,4 +26,5 @@ export interface ComercioResponse {
   cnpj?: string;
   assinaturaResponsavelBase64?: string;
   limiteCreditoPadrao?: number;
+  diaVencimentoFiado?: number;
 }
